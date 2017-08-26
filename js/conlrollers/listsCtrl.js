8162deb9da4b5ec71ac00ac96b8325c229c4982a
olegsugatov <oleg.sugatov@gmail.com>
2017-08-27 01:42:37 +0300

    +create/remove lists

--- js/conlrollers/listsCtrl.js
+++ js/conlrollers/listsCtrl.js
@@ -1,4 +1,10 @@
 angular.module('app').controller('listsCtrl', function (listFactory){
 	console.log('listsCtrl');
 	this.lists = listFactory.getLists();
+
+	this.addList = function () {
+		console.log('this.listName', this.listName);
+		listFactory.addList(this.listName);
+		this.listName = '';
+	};
 });
--- js/factories/listFactory.js
+++ js/factories/listFactory.js
@@ -19,6 +19,17 @@ angular.module('app').factory('listFactory', function () {
 	service.getLists = function () {
 		return lists;
 	};
-	
+
+	service.addList = function (listName) {
+		lists.push({
+			id: _.uniqueId('list_'),
+			listName: listName
+		});
+	};
+
+	service.removeList = function (list) {
+		_.pull(lists, list)
+	};
+
 	return service;
 });
